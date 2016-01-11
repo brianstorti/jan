594ef63fb4155c494ed77daf362cebdebcdad81c
Brian Thomas Storti <btstorti@gmail.com>
2016-01-11 21:52:13 +0100

    I can haz real time communication

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -4,4 +4,18 @@ defmodule Jan.RoomChannel do
   def join("rooms:" <> room_id, _params, socket) do
     {:ok, socket}
   end
+
+  def handle_in("new_move", %{"body" => body}, socket) do
+    # broadcast!/3 will notify all joined clients on this socket's topic and
+    # invoke their handle_out/3 callbacks
+    broadcast! socket, "new_move", %{"body" => body}
+    {:noreply, socket}
+  end
+
+  # handle_out/3 isn't a required callback, but it allows us to customize and
+  # filter broadcasts before they reach each client
+  def handle_out("new_move", payload, socket) do
+    push socket, "new_move", payload
+    {:noreply, socket}
+  end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -8,11 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
   socket.connect()
 
   let roomName = document.getElementById('room-name').value
-
   let channel = socket.channel("rooms:" + roomName, {})
+
   channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  $('.button1').on("click", event => {
+    channel.push("new_move", { body: "foo" })
+  });
+
+  channel.on("new_move", payload => {
+    $('.moves_container').append("foo")
+  })
 });
 
 export default socket
