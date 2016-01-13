ccf7f986ca0419b8cc1ba9fe133875c760e8f918
Brian Thomas Storti <btstorti@gmail.com>
2016-01-13 22:24:33 +0100

    Join room

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,21 +1,32 @@
 defmodule Jan.RoomChannel do
   use Phoenix.Channel
 
-  def join("rooms:" <> room_id, _params, socket) do
+  intercept ["new_player_joined"]
+
+  def join("rooms:" <> room_id, params, socket) do
+    send self, {:new_player_joined, Map.fetch!(params, "player_name")}
     {:ok, socket}
+  end
+
+  def handle_info({:new_player_joined, player_name}, socket) do
+    broadcast! socket, "new_player_joined", %{name: player_name}
+    {:noreply, socket}
+  end
+
+  # handle_out/3 isn't a required callback, but it allows us to customize and
+  # filter broadcasts before they reach each client
+  def handle_out("new_player_joined", payload, socket) do
+    players = [payload.name | socket.assigns[:players] || []]
+    socket = assign(socket, :players, players)
+
+    push socket, "new_player_joined", %{players: players}
+    {:noreply, socket}
   end
 
   def handle_in("new_move", %{"body" => body}, socket) do
     # broadcast!/3 will notify all joined clients on this socket's topic and
     # invoke their handle_out/3 callbacks
     broadcast! socket, "new_move", %{"body" => body}
-    {:noreply, socket}
-  end
-
-  # handle_out/3 isn't a required callback, but it allows us to customize and
-  # filter broadcasts before they reach each client
-  def handle_out("new_move", payload, socket) do
-    push socket, "new_move", payload
     {:noreply, socket}
   end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,24 +3,48 @@ import {Socket} from "phoenix"
 let socket = new Socket("/socket")
 
 document.addEventListener('DOMContentLoaded', function() {
-  if (!document.getElementById('rooms')) return;
+  $('.game').hide();
 
+  $('#join-room-form').on("submit", event => {
+    event.preventDefault();
+    init();
+  });
+});
+
+let init = function() {
   socket.connect()
 
   let roomName = document.getElementById('room-name').value
-  let channel = socket.channel("rooms:" + roomName, {})
+  let playerName = document.getElementById('player-name').value
+  let channel = socket.channel("rooms:" + roomName, { player_name: playerName })
 
-  channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+  channel.join().receive("ok", handleSuccessfulJoin)
+                .receive("error", handleFailedJoin)
 
   $('.button1').on("click", event => {
     channel.push("new_move", { body: "foo" })
   });
 
   channel.on("new_move", payload => {
-    $('.moves_container').append("foo")
+    $('.moves_container').append("<li>Foo</li>")
   })
-});
+
+  channel.on("new_player_joined", payload => {
+    console.log("payload", payload);
+
+    $('.players').append(`<p>${payload.name} joined</p>`)
+  })
+}
+
+let handleSuccessfulJoin = function(response) {
+  $('.game').show();
+  $('#join-room-form').hide();
+  console.log("Joined successfully", response);
+}
+
+let handleFailedJoin = function(response) {
+  console.log("Unable to join", response);
+}
+
 
 export default socket
