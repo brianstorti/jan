798651b88b4d014ec23e63075d9b1f4a2d25a80a
Brian Thomas Storti <btstorti@gmail.com>
2016-02-05 21:10:56 +0100

    Set host in config/prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :jan, Jan.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "jkp.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
