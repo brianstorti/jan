86814142fec8d43a506127cd4f4ac3c5285a6845
Brian Thomas Storti <btstorti@gmail.com>
2016-02-10 08:36:59 +0100

    Test Registry.unregister/1 and Registry.registered_rooms/0

--- test/lib/registry_test.exs
+++ test/lib/registry_test.exs
@@ -11,4 +11,19 @@ defmodule Jan.RegistryTest do
     assert first_pid == second_pid
     assert first_pid != third_pid
   end
+
+  test "unregisters processes" do
+    first_pid = Registry.where_is("foo")
+    Registry.unregister("foo")
+    second_pid = Registry.where_is("foo")
+
+    assert first_pid != second_pid
+  end
+
+  test "gets list of registered rooms" do
+    Registry.where_is("foo")
+    Registry.where_is("bar")
+
+    assert Registry.registered_rooms == ["bar", "foo"]
+  end
 end
